d78f144f8bce906ea70ac0f7e788b52a015ef6a6
Hakan Kurt <hakan.kurt@portalgrup.com>
2024-09-10 01:54:21 +0300

    created meal items

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,24 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+
 export default function MealsPage() {
     return(
-        <main>
-            <h1>Meals Page</h1>
+        <>
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, create <span className={classes.highlight}>by you</span>
+            </h1>
+            <p>Choose your favorite recipe</p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">
+                    Share Your Favorite Recipe
+                </Link>
+            </p>
+        </header>
+        <main className={classes.main}>
+            <MealsGrid meals={[]} />
         </main>
+        </>
     );
 }
