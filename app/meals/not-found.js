8202676419a121c3391d48ad59cb022cfc320d37
Hakan Kurt <hakan.kurt@portalgrup.com>
2024-09-10 18:25:45 +0300

    created not found for meals

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>Meal not found</h1>
+            <p>Requested meal is not found.</p>
+        </main>
+    )
+}
