e6f3d342a60422c8a25b0354cce4362ec8ffc9dd
Hakan Kurt <hakan.kurt@portalgrup.com>
2024-09-11 17:28:49 +0300

    updated metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,33 +4,37 @@ import Link from "next/link";
 import { getMeals } from "@/lib/meals";
 import { Suspense } from "react";
 
-async function Meals() {
-    const meals = await getMeals();
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the shared meals.",
+};
 
-    return <MealsGrid meals={meals} />;
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
 }
 
 export default async function MealsPage() {
-    const meals = await getMeals();
+  const meals = await getMeals();
 
-    return(
-        <>
-        <header className={classes.header}>
-            <h1>
-                Delicious meals, create <span className={classes.highlight}>by you</span>
-            </h1>
-            <p>Choose your favorite recipe</p>
-            <p className={classes.cta}>
-                <Link href="/meals/share">
-                    Share Your Favorite Recipe
-                </Link>
-            </p>
-        </header>
-        <main className={classes.main}>
-            <Suspense fallback={<p className={classes.loading}>Fetching meals</p>}>
-                <Meals />
-            </Suspense>
-        </main>
-        </>
-    );
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, create{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
